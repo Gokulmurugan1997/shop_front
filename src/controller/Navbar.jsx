de5f63033f4ad06e7272b0b3148f7650fea6176f
Gokulmurugan1997 <Gokulanshvi@gmail.com>
2024-11-24 00:49:25 +0900

    update

--- src/controller/Navbar.jsx
+++ src/controller/Navbar.jsx
@@ -2,29 +2,34 @@ import React from 'react'
 import Logout from './Logout'
 import { useNavigate } from 'react-router-dom'
 
-function Navbar({cart,setcart}) {
-    let navigate = useNavigate()
-  return <>
-   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container px-4 px-lg-5">
-                <a className="navbar-brand" href="#!">Shop</a>
-                <button className="btn btn-outline-dark" onClick={()=>navigate('/seller/login')}>
-                            Sell Product
-                </button>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse cart" id="navbarSupportedContent">
-                    <form className="d-flex">
-                        <button className="btn btn-outline-dark" onClick={()=>navigate('/mycart')}>
-                            <i className="bi-cart-fill me-1"></i>
-                            Cart
-                            <span className="badge bg-dark text-white ms-1 rounded-pill">{cart}</span>
-                        </button>
-                    </form>
+function Navbar({cart, setcart}) {
+    let navigate = useNavigate();
+    const cartCount = Array.isArray(cart) ? cart.length : cart; // Assuming cart can be an array or number
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container px-4 px-lg-5">
+                    <a className="navbar-brand" href="#!">Shop</a>
+                    <button className="btn btn-outline-dark" onClick={() => navigate('/seller/login')}>
+                        Sell Product
+                    </button>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse cart" id="navbarSupportedContent">
+                        <form className="d-flex">
+                            <button className="btn btn-outline-dark" onClick={() => navigate('/mycart')}>
+                                <i className="bi-cart-fill me-1"></i>
+                                <span className="badge bg-dark text-white ms-1 rounded-pill">Cart {cartCount}</span>
+                            </button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-            <Logout/>
-        </nav>
-  
-  </>
+                <Logout /> {/* Assuming this is a component that handles logging out */}
+            </nav>
+        </>
+    );
 }
-export default Navbar
+
+export default Navbar;
