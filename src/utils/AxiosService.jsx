1bc236dc987e63bd2663f4845df09331f04c48f1
Gokulmurugan1997 <Gokulanshvi@gmail.com>
2024-11-23 15:58:52 +0900

    update

--- src/utils/AxiosService.jsx
+++ src/utils/AxiosService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let AxiosService = axios.create({
-    baseURL:"http://localhost:8001",
+    baseURL:"https://shop-back-ioot.onrender.com",
     headers:{
         "Content-Type":"application/json"
     }
